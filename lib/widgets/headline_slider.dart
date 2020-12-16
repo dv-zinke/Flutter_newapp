54d6022bcdb55451219dd9681246413030110d07
hj <dv.zinke@gmail.com>
2020-12-16 22:32:50 +0900

    feat: headline bloc 수정 및 widget 추가

---
+++ lib/widgets/headline_slider.dart
@@ -0,0 +1,129 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/bloc/get_top_headlines_bloc.dart';
+import 'package:news_app/elements/error_element.dart';
+import 'package:news_app/elements/loader_element.dart';
+import 'package:news_app/model/article.dart';
+import 'package:news_app/model/article_response.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class HeadlineSliderWidget extends StatefulWidget {
+  @override
+  _HeadlineSliderWidgetState createState() => _HeadlineSliderWidgetState();
+}
+
+class _HeadlineSliderWidgetState extends State<HeadlineSliderWidget> {
+  @override
+  void initState() {
+    super.initState();
+    getTopHeadlinesBloc..getHeadlines();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<ArticleResponse>(
+      stream: getTopHeadlinesBloc.subject.stream,
+      builder: (context, AsyncSnapshot<ArticleResponse> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data.error != null && snapshot.data.error.length > 0) {
+            return buildErrorWidget(snapshot.data.error);
+          }
+          return _buildHeadlineSlider(snapshot.data);
+        } else if (snapshot.hasError) {
+          return buildErrorWidget(snapshot.error);
+        } else {
+          return buildLoadingWidget();
+        }
+      },
+    );
+  }
+
+  Widget _buildHeadlineSlider(ArticleResponse data) {
+    List<ArticleModel> articles = data.articles;
+    return Container(
+      child: CarouselSlider(
+        options: CarouselOptions(
+            enlargeCenterPage: false, height: 200, viewportFraction: 0.9),
+        items: getExpenseSliders(articles),
+      ),
+    );
+  }
+
+  getExpenseSliders(List<ArticleModel> articles) {
+    return articles.map((article) => GestureDetector(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: article.img == null
+                              ? AssetImage("assets/img/placeholder.jpg")
+                              : NetworkImage(article.img))),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          stops: [
+                            0.1,
+                            0.9
+                          ],
+                          colors: [
+                            Colors.black.withOpacity(0.9),
+                            Colors.white.withOpacity(0.0)
+                          ])),
+                ),
+                Positioned(
+                  bottom: 30,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    width: 25,
+                    child: Column(
+                      children: [
+                        Text(
+                          article.title,
+                          style: TextStyle(
+                              height: 1.5,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Text(
+                    article.source.title,
+                    style: TextStyle(color: Colors.white54, fontSize: 9),
+                  ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  child: Text(
+                    timeAgo(DateTime.parse(article.date)),
+                    style: TextStyle(color: Colors.white54, fontSize: 9),
+
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  String timeAgo(DateTime date) {
+    return timeago.format(date, allowFromNow: true, locale: 'en');
+  }
+}
